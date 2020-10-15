c387d925cdac227514023a604883ca3ecb5954ee
Neema98 <neems37@gmail.com>
2020-10-15 14:56:09 +0530

    Third commit

--- MyApp/src/main/java/com/ofss/HelloWorld.java
+++ MyApp/src/main/java/com/ofss/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First program in Git");
+		Hi h=new Hi("Neema",9879876);
+		System.out.println(h);
 
 	}
 
--- MyApp/src/main/java/com/ofss/Hi.java
+++ MyApp/src/main/java/com/ofss/Hi.java
@@ -4,5 +4,28 @@ public class Hi {
 
 	String name;
 	long phoneNumber;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	@Override
+	public String toString() {
+		return "Hi [name=" + name + ", phoneNumber=" + phoneNumber + "]";
+	}
+	public Hi(String name, long phoneNumber) {
+		super();
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
 
 }
